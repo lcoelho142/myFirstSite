abf563eaf2600b582409a385474164c658961c06
Leah Coelho <lcoelho142@gmail.com>
2026-01-09 18:52:01 -0500

    Added new project to site
    - added WeatherApp project
    - added splash screen for work-in-progress alert on first site visit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert("Hello World");
+// alert("Website Development in Progress!");
+
+// Site Loading Screen
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+
+    if (document.documentElement.classList.contains('show-loader')) {
+    
+        setTimeout(() => {
+            loader.classList.add('fade-out');
+            sessionStorage.setItem('hasSeenSplash', 'true');
+
+            setTimeout(() => {
+                loader.style.display = 'none';
+                document.documentElement.classList.remove('show-loader');
+            }, 1500);
+        }, 2000);
+    }
+});
